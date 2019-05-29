4253f05abe4dae967764441e58b62b7fc29fe308
Jose Barron Tovar <josebarrontovar@hotmail.com>
2019-05-29 10:31:39 -0500

    first commit

---
+++ App4/App4/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace App4
+{
+    [Activity(Label = "Widget THD Test...", MainLauncher = true, Icon = "@mipmap/android")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+   
+        }
+    }
+}
---
+++ App4/App4/obj/Debug/90/android/src/md591ac425b7bebc7a65cec0ef28027c2db/AppWidget.java
@@ -0,0 +1,56 @@
+package md591ac425b7bebc7a65cec0ef28027c2db;
+
+
+public class AppWidget
+	extends android.appwidget.AppWidgetProvider
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onUpdate:(Landroid/content/Context;Landroid/appwidget/AppWidgetManager;[I)V:GetOnUpdate_Landroid_content_Context_Landroid_appwidget_AppWidgetManager_arrayIHandler\n" +
+			"n_onReceive:(Landroid/content/Context;Landroid/content/Intent;)V:GetOnReceive_Landroid_content_Context_Landroid_content_Intent_Handler\n" +
+			"";
+		mono.android.Runtime.register ("App4.AppWidget, App4", AppWidget.class, __md_methods);
+	}
+
+
+	public AppWidget ()
+	{
+		super ();
+		if (getClass () == AppWidget.class)
+			mono.android.TypeManager.Activate ("App4.AppWidget, App4", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onUpdate (android.content.Context p0, android.appwidget.AppWidgetManager p1, int[] p2)
+	{
+		n_onUpdate (p0, p1, p2);
+	}
+
+	private native void n_onUpdate (android.content.Context p0, android.appwidget.AppWidgetManager p1, int[] p2);
+
+
+	public void onReceive (android.content.Context p0, android.content.Intent p1)
+	{
+		n_onReceive (p0, p1);
+	}
+
+	private native void n_onReceive (android.content.Context p0, android.content.Intent p1);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
